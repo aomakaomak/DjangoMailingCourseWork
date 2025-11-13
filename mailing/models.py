cb110651e84ae2b945f3f48c373a4834f4b10756
Andrei Makarov <aomak@yandex.ru>
2025-11-13 18:20:32 +0300

    созданы модели клиент и сообщение

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    email = models.EmailField(verbose_name='Почта', unique=True)
+    full_name = models.CharField(max_length=150, verbose_name='ФИО')
+    comment = models.TextField(blank=True, null=True, verbose_name='Комментарий')
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=100, verbose_name='Тема письма')
+    text = models.TextField(verbose_name='Текст')
+
+
+
