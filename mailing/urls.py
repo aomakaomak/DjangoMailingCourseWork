75ad8eeac8c2ec77871ff97f5c01412d077198e3
Andrei Makarov <aomak@yandex.ru>
2025-11-13 17:22:04 +0300

    создал приложение mailing

---
+++ mailing/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+# from .views import
+
+app_name = 'mailing'
+
+urlpatterns = [
+    # path('home/', ProductListView.as_view(), name='home'),
+]
