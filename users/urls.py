423318ace694b43a0716197f06036ee26c3215df
Andrei Makarov <aomak@yandex.ru>
2025-11-16 00:12:17 +0300

    добавлено подтверждение по почте

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import RegisterView
+from .views import RegisterView, activate_account
 
 
 app_name = 'users'
@@ -10,5 +10,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='mailing:index'), name='logout'),
+
+    path('activate/<uidb64>/<token>/', activate_account, name='activate'),
 
 ]
