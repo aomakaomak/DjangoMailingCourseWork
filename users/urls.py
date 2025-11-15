0b7f0b535b29ed9470fe8f5a6bc113d2471f8521
Andrei Makarov <aomak@yandex.ru>
2025-11-16 00:25:42 +0300

    добавлено восстановление пароля

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 from .views import RegisterView, activate_account
 
@@ -12,5 +12,46 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='mailing:index'), name='logout'),
 
     path('activate/<uidb64>/<token>/', activate_account, name='activate'),
+
+# 1. форма "забыли пароль?"
+    path(
+        'password_reset/',
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html',
+            email_template_name='users/password_reset_email.html',
+            subject_template_name='users/password_reset_subject.txt',
+            success_url=reverse_lazy('users:password_reset_done'),
+        ),
+        name='password_reset',
+    ),
+
+    # 2. страница "письмо отправлено"
+    path(
+        'password_reset/done/',
+        PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html',
+        ),
+        name='password_reset_done',
+    ),
+
+    # 3. страница по ссылке из письма, ввод нового пароля
+    path(
+        'reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html',
+            success_url=reverse_lazy('users:password_reset_complete'),
+        ),
+        name='password_reset_confirm',
+    ),
+
+    # 4. финальная страница "пароль изменён"
+    path(
+        'reset/done/',
+        PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html',
+        ),
+        name='password_reset_complete',
+    ),
+
 
 ]
